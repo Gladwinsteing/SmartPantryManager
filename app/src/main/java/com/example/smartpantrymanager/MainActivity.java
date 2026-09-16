ca0e7156eb2c8444a86b1b02c97822afe391060b
Gladwin <GMAPHUTI6@GMAIL.COM>
2026-09-16 22:14:05 +0200

    Added imports for utils package

--- app/src/main/java/com/example/smartpantrymanager/MainActivity.java
+++ app/src/main/java/com/example/smartpantrymanager/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.util.Log;
+import com.example.smartpantrymanager.utils.*;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
