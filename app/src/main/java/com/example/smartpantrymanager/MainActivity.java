b156e5468d7c2c45ca6221fa82572dc21c0b9838
Gladwin <GMAPHUTI6@GMAIL.COM>
2026-09-16 22:57:31 +0200

    Reset MainActivity back to default code

--- app/src/main/java/com/example/smartpantrymanager/MainActivity.java
+++ app/src/main/java/com/example/smartpantrymanager/MainActivity.java
@@ -8,9 +8,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.util.Log;
-import com.example.smartpantrymanager.utils.*;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
